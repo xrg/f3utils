b6339256836fd90ee4fefb959ec3853d39d1409a
P. Christeas <xrg@pefnos.com>
2025-10-19 14:56:21 +0300

    [ctx_logging] allow picking contents of context variable
    Sometimes the value of context variable is structured and we only want
    to log a member of that structure. Allow supplying a function to pick
    whatever we'd want from that structure.
    
    Also improve the docstring and examples.

--- f3utils/context_logging.py
+++ f3utils/context_logging.py
@@ -3,7 +3,9 @@
 
 import logging
 import contextvars
-from typing import Optional
+
+from collections.abc import Callable
+from typing import Any, Optional
 
 
 class AddLogContext(logging.Filter):
@@ -12,33 +14,73 @@ class AddLogContext(logging.Filter):
     Add this as a filter to logger handers of any level, in order to pull some
     context var and insert it as extra attributes to the LogRecord being emitted.
 
-    Example:
+    :param ctx_var: reference to ContextVar that should be used
+    :param var_name: in simple mode, the attribute name on the LogRecord to set
+    :param setter: a function, that sets one or more LogRecord attributes after
+                    the value of context var
 
-        request = contextvars.ContextVar("request")
+
+    Hint: use the 'setter' if the context var content would be too detailed to
+    use directly into log formatting or journald structure.
+
+    Example::
+
+        cur_request = contextvars.ContextVar("request")
 
         def init_logging():
             app_log = logging.getLogger("app")
             app_hander = logging.StreamHandler()
-            app_handler.addFilter(AddLogContext(request))
+            app_handler.addFilter(AddLogContext(cur_request, 'request'))
 
 
         ...
 
         def handle_some_stuff():
-            # here, if 'request' has a value, it will be set to the LogRecord
+            # here, if 'cur_request' has a value, it will be set to the LogRecord
+            # as 'record.request = ...'
+
             log.info("I got some stuff")
 
+
+    Example 2 (with setter) ::
+
+        from requests import Request
+
+        cur_request: ContextVar[Request] = ContextVar("request")
+
+        def _record_decorator(record, req_value):
+            record.req_method = req_value.method
+            record.req_url = req_value.url
+
+        def init_logging():
+            app_log = logging.getLogger("app")
+            app_hander = logging.StreamHandler()
+            app_handler.addFilter(AddLogContext(cur_request, setter=_record_decorator))
+
+        # now, each log record may have:
+        # record.req_method = "GET"
+        # record.req_url = "https://example.com/foo?..."
     """
 
-    def __init__(self, ctx_var: contextvars.ContextVar, var_name: Optional[str] = None):
+    def __init__(self, ctx_var: contextvars.ContextVar,
+                 var_name: Optional[str] = None,
+                 setter: Optional[Callable[[logging.LogRecord, Any], None]] = None):
         super().__init__()
-        self._var_name = var_name or ctx_var.name
         self._ctx_var = ctx_var
+        if setter:
+           self._setter = setter
+        else:
+            if not var_name:
+                var_name = ctx_var.name
+            def _setter(record, value):
+                setattr(record, var_name, value)
+
+            self._setter = _setter
 
     def filter(self, record: logging.LogRecord):
         try:
             val = self._ctx_var.get()
-            setattr(record, self._var_name, val)
+            self._setter(record, val)
         except LookupError:
             pass
         return True
